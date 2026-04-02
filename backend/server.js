8279b6abac3325067720b6b4f5b6eba2762e0475
pavan347 <pavankumargarapati04@gmail.com>
2026-04-02 16:00:46 +0530

    frontend env url moved to frist rather than localhost url

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173' || process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
